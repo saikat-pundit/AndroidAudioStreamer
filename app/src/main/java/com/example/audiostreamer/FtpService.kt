808e72a308be5d1e21f8484596f75c666bc418e2
saikat-pundit <saikatpundit@yandex.com>
2026-03-16 07:52:53 +0530

    Update FtpService.kt

--- app/src/main/java/com/example/audiostreamer/FtpService.kt
+++ app/src/main/java/com/example/audiostreamer/FtpService.kt
@@ -80,7 +80,7 @@ class FtpService : Service() {
             val userManager = serverFactory.userManager
             val user = BaseUser()
             user.name = "anonymous"
-            user.homeDirectory = "/storage"
+            user.homeDirectory = Environment.getExternalStorageDirectory().absolutePath
             
             val auths = ArrayList<org.apache.ftpserver.ftplet.Authority>()
             auths.add(WritePermission())
